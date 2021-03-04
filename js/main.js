c8c7d4ad6f3cf4cd6bf998855dc7bc097bec846a
Richard Berko <richyb98@yahoo.com>
2021-03-04 12:59:27 +0000

    updat

--- js/main.js
+++ js/main.js
@@ -13,6 +13,33 @@ console.log(a+c);
 
 console.log(a*c);
 
-let surname = "Berko"
+let surname = " Berko"
 let firstname = "Richard"
 console.log(firstname + surname);
+console.log(surname + firstname);
+
+
+// Arrays and Objects
+let myarray = [8,4,10,3,4,5,12]
+let names = ["Kofi", "Sam", "Afia"]
+let mymixed =[0,4,6, "Yaw", "yam", "banana"]
+
+//console.log(myarray[2])
+//console.log(myarray[3])
+//console.log(myarray[6])
+
+//myarray.push(50)
+//console.log(myarray);
+myarray.pop()
+console.log(myarray)
+
+//object
+let myobject = {
+    name: "Kofi", class: 3, 
+    classname:"mary Gold", 
+    result:[
+        {subject: "English", mark:40, position:1},
+        {subject; "Maths", mark: 80, position: 4},
+        {subject: "Social", mark: 50, position: 3}
+    ]
+}
