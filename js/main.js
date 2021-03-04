93bb7cb9ef7bf5495c28dac32a6abb114b61da29
Richard Berko <richyb98@yahoo.com>
2021-03-04 11:55:54 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 console.log("hellooo Richard.....")
-
+//variables
 let b=78
 
 console.log(b);
+let myname = "Richard";
+console.log(myname);
+let a = 5
+let c = 9
+console.log(a+c);
+
+
+
+console.log(a*c);
+
+let surname = "Berko"
+let firstname = "Richard"
+console.log(firstname + surname);
